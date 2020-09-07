f85f45f1ab03447a46aa2bec7719113623580db2
faithlierheimer <flierheimer@gmail.com>
2020-09-07 14:29:42 -0600

    working on flask app, but confused about calling fxns that I improt from other files

--- flaskapp.py
+++ flaskapp.py
@@ -5,9 +5,17 @@ import requests
 import pymongo
 from splinter import Browser
 from flask import Flask, render_template
+from scrape_mars import init_browser, scrape
 # Initialize PyMongo to work with MongoDBs
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 # Define database and collection
 db = client.mars_db
-collection = db.news
+collection = db.mars
+
+####Begin flask app routes######
+@app.route("/")
+
+@app.route("/scrape")
+init_browser()
+scrape()
